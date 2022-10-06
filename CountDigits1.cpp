f03377f893e2e032e2ab0894ef67b93017b94a10
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2022-10-06 23:30:47 +0530

    Create CountDigits1.cpp

---
+++ CountDigits1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<limits.h>
+#include<math.h>
+using namespace std;
+int countDigits(int x){
+  return floor(log10(x))+1;
+}
+int main(){
+  int n;
+  cin>>n;
+  cout<<countDigits(n);
+  return 0;
+}
+
+#Time complexity = O(1)
