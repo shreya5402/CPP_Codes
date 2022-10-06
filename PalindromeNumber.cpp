82359a9bb82f9b860ce3da7fa4a8221a5f6a179d
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2022-10-06 23:38:17 +0530

    Create PalindromeNumber.cpp

---
+++ PalindromeNumber.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<limits.h>
+#include<math.h>
+using namespace std;
+int isPal(int x){
+  int rev=0;
+  int temp=x;
+  while(temp!=0){
+      int ld=temp%10;
+      rev=rev*10+ld;
+      temp=temp/10;
+  }
+  return rev==x;
+}
+int main(){
+  int n;
+  cin>>n;
+  cout<<(bool)isPal(n);
+  return 0;
+}
+
+#Time complexity is theta(d) where is the number of digits in the input.
