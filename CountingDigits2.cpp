787130f745208ada939724acb1ca9df17d04d420
Shreya Pandey <74672603+shreya5402@users.noreply.github.com>
2023-01-16 16:00:02 +0530

    Update CountingDigits2.cpp

--- CountingDigits2.cpp
+++ CountingDigits2.cpp
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
-int countDigits(int x){
+int countDigits(long long int x){
+  if(x==0)
+    return 1;
   int res=0;
-  while(x>0){
+  while(x!=0){
       x=x/10;
       res++;
   }
   return res;
 }
-int main(){
-  int n;
+int main(void){
+  long long n;
   cin>>n;
   cout<<countDigits(n);
   return 0;
